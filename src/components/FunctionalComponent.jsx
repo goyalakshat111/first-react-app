d1508d0014a8fce5adad2c875c77dcf1251e8171
Akshat <goyalakshat111@gmail.com>
2022-12-20 11:42:02 +0530

    States and Props in React.js

---
+++ src/components/FunctionalComponent.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+const FunctionalComponent = (props) => {
+
+    const [count, setCount] = useState(0);
+    const [changeName, setChangeName] = useState("");
+
+    const subtractConst = () => {
+        setCount(count - 1);
+    }
+
+    const { name, age, company, setName } = props;
+
+
+    return (
+        <div>
+            <p>This is Functional Component</p>
+            <button onClick={() => setCount(count + 1)} >Click me to Add 1</button>
+            <button onClick={subtractConst} >Click me to Subtract 1</button>
+            <h1>
+                {count}
+            </h1>
+            <h1>My name is {name} My age is {age}, I work for {company}</h1>
+            <input onChange={(e) => setChangeName(e.target.value)} />
+            <button onClick={() => setName(changeName)}>Change name to Ram</button>
+        </div>
+    );
+}
+
+export default FunctionalComponent;
